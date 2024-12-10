5760e48848a5fad73e773db56a7f1d965bed4c7d
Nurzhan2023 <nurzhan02061990@gmail.com>
2024-12-10 11:59:48 +0500

    brain-even project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Nurzhan2023/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Nurzhan2023/python-project-49/actions)
+
+
+https://asciinema.org/connect/9023391f-0a2a-4d77-92f4-c89667a988b0
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,46 @@
+# brain_games/scripts/brain_even.py
+import prompt
+import random
+
+
+def main():
+    ROUNDS_COUNT = 3
+
+
+    def generate_round():
+        number = random.randint(1, 100)
+        question = str(number)
+        correct_answer = "yes" if number %2 == 0 else "no"
+        return question, correct_answer
+
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name?")
+    print(f"Hello, {name}!")
+    print('Answer "Yes"  if the number is even, otherwise answer "no".')
+
+
+    for i in range(ROUNDS_COUNT):
+        question, correct_answer = generate_round()
+        print(f"Question: {question}")
+        answer = prompt.string("Your answer: ").strip().lower()
+
+        if answer != correct_answer:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.)")
+            print(f"Let's try again, {name}!")
+            return
+
+        else:
+             print("Correct!")
+
+
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
