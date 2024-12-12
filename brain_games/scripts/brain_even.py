f512ab3879c057ac74c41806c419297e1830d228
Nurzhan2023 <150884350+Nurzhan2023@users.noreply.github.com>
2024-12-12 18:38:48 +0600

    Update brain_even.py update

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,17 +1,15 @@
 # brain_games/scripts/brain_even.py
-import prompt
 import random
+
+import prompt
 
 
 def main():
     ROUNDS_COUNT = 3
-
-
     print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name?")
+    name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
     print('Answer "yes" if the number is even, otherwise answer "no".')
-
 
     for i in range(ROUNDS_COUNT):
         question, correct_answer = generate_round()
@@ -19,14 +17,15 @@ def main():
         answer = prompt.string("Your answer: ").strip().lower()
 
         if answer != correct_answer:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(
+                f"'{answer}' is wrong answer ;"
+                "(. Correct answer was '{correct_answer}'."
+            )
             print(f"Let's try again, {name}!")
             return
 
-
         else:
             print("Correct!")
-
     
     print(f"Congratulations, {name}!")
 
