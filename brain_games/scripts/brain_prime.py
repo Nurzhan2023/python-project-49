1c9bb163df6794f746baeb28bc2244c3c6e92cac
Nurzhan2023 <nurzhan02061990@gmail.com>
2024-12-12 15:37:14 +0500

    brain-prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -7,7 +7,7 @@ import prompt
 
 
 def main():
-    ROUNDS_COUNT = 1
+    ROUNDS_COUNT = 3
 
 
     print("Welcome to the Brain Games!")
@@ -28,12 +28,12 @@ def main():
 
         else:
             print("Correct!")
-
+    print(f"Congratulations, {name}!")
 
 def is_prime(n):
     if n<=1:
         return False
-    for i in range(1, int(n**0.5)+1):
+    for i in range(2, int(n**0.5)+1):
         if n%i == 0:
             return False
 
