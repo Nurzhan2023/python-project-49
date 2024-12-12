ab7c65e2d3daa6a21666cc0dd991aaee24e6103f
Nurzhan2023 <150884350+Nurzhan2023@users.noreply.github.com>
2024-12-12 18:42:07 +0600

    Update brain_even.py update

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -33,7 +33,7 @@ def main():
 def generate_round():
     number = random.randint(1, 100)
     question = str(number)
-    correct_answer = "yes" if number %2 == 0 else "no"
+    correct_answer = "yes" if number % 2 == 0 else "no"
     return question, correct_answer
 
 
