942a1b3a8aec4d20e1f1525f1f840933ff345d5b
Nurzhan2023 <150884350+Nurzhan2023@users.noreply.github.com>
2024-12-12 18:28:56 +0600

    Update brain_calc.py+

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -3,11 +3,6 @@ import random
 
 import prompt
 
-# brain_games/scripts/brain_calc.py
-import random
-
-import prompt
-
 
 def main():
     ROUNDS_COUNT = 3
@@ -47,59 +42,6 @@ def randomaizer():
         result = number1 * number2
 
     return question, str(result)
-
-
-if __name__ == "__main__":
-    main()
-
-def main():
-    
-    ROUNDS_COUNT = 3
-    
-    print("Welcome to the Brain Games!")
-    
-    name = prompt.string('May I have your name?')
-    print(f"Hello, {name}!")
-    print("What is the result of the expression?")
-
-    for i in range(ROUNDS_COUNT):
-        
-        question, result = randomaizer()
-        print(f"Question: {question}")
-        answer = prompt.string("Your answer: ").strip().lower()
-
-        if answer != result:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'.) ")
-            print(f"Let's try again, {name}!")
-            return
-
-        else:
-            print("Correct!")
-
-    print(f"Congratulations, {name}!")
-
-
-
-
-
-
-def randomaizer():
-    number1 = random.randint(1, 100)
-    number2 = random.randint(1, 100)
-    operator = random.choice(['+', '-', '*'])
-    if operator == '+':
-        question = (f"{number1} {operator} {number2}")
-        result = number1 + number2
-    elif operator == '-':
-        question = (f"{number1} {operator} {number2}")
-        result = number1 - number2
-    elif operator == '*':
-        question = (f"{number1} {operator} {number2}")
-        result = number1 * number2
-    
-    return question, str(result)
-
-
 
 
 if __name__ == "__main__":
