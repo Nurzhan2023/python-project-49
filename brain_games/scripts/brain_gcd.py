5e717ee98c83fcfde52a84f7c7c1dd7cd67ebe46
Nurzhan2023 <150884350+Nurzhan2023@users.noreply.github.com>
2024-12-12 18:43:52 +0600

    Update brain_gcd.py update

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,22 +1,20 @@
 # brain_games/scripts/brain_gcd.py
-
 import random
+
 import prompt
 import math
 
+
 def main():
     ROUNDS_COUNT = 3
-
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name?')
     print(f"Hello, {name}!")
     print ("Find the greatest common divisor of given numbers.")
 
     for i in range(ROUNDS_COUNT):
-
         number1, number2 = find_divisor()
         correct_answer = math.gcd(number1, number2)
-
         print(f"Question: {number1} {number2}")
         user_answer = input("Your answer: ")
 
@@ -24,27 +22,20 @@ def main():
             print("Correct!")
 
         else:
-            print(f"'{user_answer}' is wrong answer ;(.Correct answer was '{correct_answer}'. ")
+            print(
+                f"'{user_answer}' is wrong answer ;"
+                "(.Correct answer was '{correct_answer}'. "
+            )
             print(f"Let's try again, {name}!")
             return
+            
     print(f"Congratulations, {name}!")
-
-
-
 
 
 def find_divisor():
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
     return number1, number2 
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
