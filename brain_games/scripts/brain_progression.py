fd967a9274e3b8b03f64ece3acd83b0a49306201
Nurzhan2023 <150884350+Nurzhan2023@users.noreply.github.com>
2024-12-12 19:05:23 +0600

    Update brain_progression.py update

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -36,7 +36,7 @@ def generate_progression():
     step = random.randint(2, 10)
     length = random.randint(5, 10)
     progression = [start + i * step for i in range(length)]
-    hidden_index = random.randint(0, length -1)
+    hidden_index = random.randint(0, length - 1)
     hidden_value = progression[hidden_index]
     progression[hidden_index] = ".."
     return progression, hidden_value
